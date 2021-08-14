844b791dcb17dc705ad3d04865b6bca3363e8e6e
Ocean <oceanacademypuducherry@gmail.com>
2021-08-14 21:25:34 +0530

    updated design

--- src/Screens/Query.js
+++ src/Screens/Query.js
@@ -1,7 +1,117 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Slide,
+  TextField,
+} from "@material-ui/core";
 
-function Query() {
-  return <h1>Query</h1>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function Query() {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button
+        variant="outlined"
+        color="primary"
+        style={{
+          marginLeft: "90%",
+        }}
+        onClick={handleClickOpen}
+      >
+        + Add Query
+      </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+      >
+        <DialogTitle>{"Query"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <div className={classes.root}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <TextField
+                      id="outlined-basic"
+                      label="Related Image"
+                      variant="outlined"
+                      style={{ width: "100%", marginLeft: "0%", margin: "1%" }}
+                    />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <TextField
+                      id="outlined-basic"
+                      label="Image"
+                      variant="outlined"
+                      style={{ width: "100%", marginLeft: "0%", margin: "1%" }}
+                    />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <TextField
+                      id="outlined-basic"
+                      label="Image"
+                      variant="outlined"
+                      style={{ width: "100%", marginLeft: "0%", margin: "1%" }}
+                    />
+                  </Paper>
+                </Grid>
+              </Grid>
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleClose}
+            variant="contained"
+            style={{
+              background: "#1F6DE2",
+              width: "100%",
+              height: "55px",
+              color: "white",
+            }}
+          >
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
-export default Query;
-Query;
